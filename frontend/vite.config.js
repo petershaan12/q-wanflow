6671790b04450525b146710a39726f6d0f3d2b92
= <=>
2026-03-27 09:41:35 +0700

    update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,16 +6,5 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 5173,
-    proxy: {
-      '/api': {
-        // target: 'http://localhost:8000',
-        target: 'http://43.129.49.243:8080',
-        changeOrigin: true,
-      },
-      '/static': {
-        target: 'http://43.129.49.243:8080',
-        changeOrigin: true,
-      },
-    },
   },
 })
